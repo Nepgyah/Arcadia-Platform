9c5edb196a24b98e28206aaa67207e33845c2271
Auston <nepgyah@gmail.com>
2026-02-16 00:28:33 -0600

    feat: add side drawers to top nav

--- src/components/navigation/topNav.tsx
+++ src/components/navigation/topNav.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { apiGET } from "@/utils/api/get";
-import { Avatar } from "@chakra-ui/react";
+import { Avatar, Button, Drawer, IconButton } from "@chakra-ui/react";
 import { useEffect } from "react";
-
+import { Hamburger } from "lucide-react";
 export default function TopNav() {
     
     useEffect(() => {
@@ -20,12 +20,44 @@ export default function TopNav() {
                 <img src="/logos/logo_white.svg" alt="" />
             </div>
             <div className="hamburger">
-                burger
+                <Drawer.Root placement={'start'}>
+                    <Drawer.Trigger asChild>
+                        <IconButton variant={'ghost'} size={'lg'}>
+                            <Hamburger />
+                        </IconButton>
+                    </Drawer.Trigger>
+                    <Drawer.Positioner>
+                        <Drawer.Content>
+                            <Drawer.Header>
+                                Navigation
+                            </Drawer.Header>
+                            <Drawer.Body></Drawer.Body>
+                        </Drawer.Content>
+                    </Drawer.Positioner>
+                </Drawer.Root>
             </div>
             <div className="profile">
-                <Avatar.Root>
-                    <Avatar.Image src={'/sad.jpeg'} />
-                </Avatar.Root>
+                <Drawer.Root>
+                    <Drawer.Trigger asChild>
+                        <Avatar.Root>
+                            <Avatar.Image src={'/sad.jpeg'} />
+                        </Avatar.Root>
+                    </Drawer.Trigger>
+                    <Drawer.Positioner>
+                        <Drawer.Content>
+                            <Drawer.Header>
+                                <p>My Profile</p>
+                                <Avatar.Root>
+                                    <Avatar.Image src={'/sad.jpeg'} />
+                                </Avatar.Root>
+                            </Drawer.Header>
+                            <Drawer.Body></Drawer.Body>
+                            <Drawer.Footer>
+                                Logout
+                            </Drawer.Footer>
+                        </Drawer.Content>
+                    </Drawer.Positioner>
+                </Drawer.Root>
             </div>
         </div>
     )
