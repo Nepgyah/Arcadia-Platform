4fd37870e1df62f949c460640e797c41f4cff139
Auston <nepgyah@gmail.com>
2026-02-24 21:10:53 -0600

    fix: pr changes

--- src/components/navigation/topNav.tsx
+++ src/components/navigation/topNav.tsx
@@ -41,47 +41,12 @@ export default function TopNav() {
         })
     }
 
-import { Avatar, Button, Drawer, IconButton } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { Hamburger } from "lucide-react";
-import { d2xAPI } from "@/utils/api/d2xAPI";
-import { arcadiaAPI } from "@/utils/api/arcadiaAPI";
-export default function TopNav() {
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            
-            const res =  await d2xAPI.GET('auth/exchange/')
-            // await arcadiaAPI.GET('util/csrf/')
-        }
-
-        fetchData()
-    })
-
     return (
         <div id="top-nav">
             <div className="icon">
                 <Link href={'/'}>
                     <img src="/logos/logo_white.svg" alt="" />
                 </Link>
-                <img src="/logos/logo_white.svg" alt="" />
-            </div>
-            <div className="hamburger">
-                <Drawer.Root placement={'start'}>
-                    <Drawer.Trigger asChild>
-                        <IconButton variant={'ghost'} size={'lg'}>
-                            <Hamburger />
-                        </IconButton>
-                    </Drawer.Trigger>
-                    <Drawer.Positioner>
-                        <Drawer.Content>
-                            <Drawer.Header>
-                                Navigation
-                            </Drawer.Header>
-                            <Drawer.Body></Drawer.Body>
-                        </Drawer.Content>
-                    </Drawer.Positioner>
-                </Drawer.Root>
             </div>
             <div className="profile">
                 <Drawer.Root>
@@ -93,7 +58,6 @@ export default function TopNav() {
                                 :
                                     <Avatar.Image src={'/sad.jpeg'} />
                             }
-                            <Avatar.Image src={'/sad.jpeg'} />
                         </Avatar.Root>
                     </Drawer.Trigger>
                     <Drawer.Positioner>
@@ -117,15 +81,6 @@ export default function TopNav() {
                                         </Button>
                                 }
                             </Drawer.Body>
-                            {/* <Drawer.Footer>
-                                Logout
-                            </Drawer.Footer> */}
-                                <p>My Profile</p>
-                                <Avatar.Root>
-                                    <Avatar.Image src={'/sad.jpeg'} />
-                                </Avatar.Root>
-                            </Drawer.Header>
-                            <Drawer.Body></Drawer.Body>
                             <Drawer.Footer>
                                 Logout
                             </Drawer.Footer>
