b816315d8608dd3474ccf3b4c51b2bd215ce1f38
Auston <137665935+Nepgyah@users.noreply.github.com>
2026-02-24 20:33:07 -0600

    Feature/miru-v0.1
    * feat: add navigation layouts
    
    * feat: add side drawers to top nav
    
    * feat: add miru layout
    
    * feat: add path url calculation
    
    * feat: creat d2xAPI class
    
    * feat: add graphql function
    
    * feat: add info-item component
    
    * feat: add base anime detail content
    
    * feat: update custom components
    
    * feat: add franchise section on anime details
    
    * feat: add miru homepage
    
    * feat: add stuff (sorry)
    
    * feat: add miru anime list page
    
    * feat: add demo account handling

---
+++ src/app/(main)/miru/list/[user_id]/page.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import Date from "@/components/custom/date";
+import Header from "@/components/custom/header";
+import { arcadiaAPI } from "@/utils/api/arcadiaAPI";
+import { Table } from "@chakra-ui/react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+    const params = useParams<{ user_id: string}>()
+    const [watchlist, setWatchlist] = useState<any []>([])
+    const [completedList, setCompletedList] = useState([])
+    const [planToList, setPlanToList] = useState([])
+    const [onHoldList, setOnHoldList] = useState([])
+
+    useEffect(() => {
+        async function FetchAnimeList() {
+            const query = 
+            `
+            query {
+                getAnimeList(userId: ${params.user_id}) {
+                    watching {
+                        anime {
+                            id,
+                            slug,
+                            title
+                        },
+                        score,
+                        startWatchDate,
+                        endWatchDate
+                    },
+                    completed {
+                        anime {
+                            id,
+                            slug,
+                            title
+                        },
+                        score,
+                        startWatchDate,
+                        endWatchDate
+                    },
+                    planTo {
+                        anime {
+                            id,
+                            slug,
+                            title
+                        },
+                        score,
+                        startWatchDate,
+                        endWatchDate
+                    },
+                    onHold {
+                        anime {
+                            id,
+                            slug,
+                            title
+                        },
+                        score,
+                        startWatchDate,
+                        endWatchDate
+                    }
+                }
+            }
+            `
+
+            const results = await arcadiaAPI.GraphQL<any>(query)
+            setWatchlist(results.data.getAnimeList.watching)
+            setCompletedList(results.data.getAnimeList.completed)
+            setPlanToList(results.data.getAnimeList.planTo)
+            setOnHoldList(results.data.getAnimeList.onHold)
+        }
+
+        FetchAnimeList()
+    }, [])
+
+    return (
+        <div id="page-miru-animelist" className="page-content">
+            <div>
+                <Header text="Watching"/>
+                <AnimeList list={watchlist} />
+            </div>
+            <div>
+                <Header text="Completed"/>
+                <AnimeList list={completedList} />
+            </div>
+            <div>
+                <Header text="Plan To"/>
+                <AnimeList list={planToList} />
+            </div>
+            <div>
+                <Header text="On Hold"/>
+                <AnimeList list={onHoldList} />
+            </div>
+        </div>
+    )
+}
+
+function AnimeList({list}:{list: any[]}) {
+    return (
+        <Table.Root size={"lg"} className="arcadia-table">
+            <Table.Header>
+                <Table.Row>
+                    <Table.ColumnHeader width={'50%'}>Title</Table.ColumnHeader>
+                    <Table.ColumnHeader>Score</Table.ColumnHeader>
+                    <Table.ColumnHeader>Start Date</Table.ColumnHeader>
+                    <Table.ColumnHeader>End Date</Table.ColumnHeader>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {
+                    list ?
+                        list.length != 0 ?
+                            list.map((entry: any, idx: number) => (
+                                <Table.Row>
+                                    <Table.Cell>
+                                        <Link href={`/miru/anime/${entry.anime.id}/${entry.anime.slug}`} className="hover-underline">
+                                            {entry.anime.title}
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>{entry.score ? entry.score : '--'}</Table.Cell>
+                                    <Table.Cell>{entry.startWatchDate ? <Date dateString={entry.startWatchDate}/>  : "--"}</Table.Cell>
+                                    <Table.Cell>{entry.endWatchDate ? <Date dateString={entry.endWatchDate}/>  : "--"}</Table.Cell>
+                                </Table.Row>
+                            ))
+                        :
+                            <Table.Row>
+                                <Table.Cell colSpan={5}> No Anime Here!</Table.Cell>
+                            </Table.Row>
+                    :
+                        <Table.Row>
+                            <Table.Cell colSpan={5}>Loading</Table.Cell>
+                        </Table.Row>
+                }
+            </Table.Body>
+        </Table.Root>
+        
+
+    )
+}
--- src/components/navigation/topNav.tsx
+++ src/components/navigation/topNav.tsx
@@ -1,5 +1,45 @@
 'use client';
 
+import { Avatar, Button, Drawer, IconButton } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Hamburger } from "lucide-react";
+import { d2xAPI } from "@/utils/api/d2xAPI";
+import { arcadiaAPI } from "@/utils/api/arcadiaAPI";
+import { toaster } from "../ui/toaster";
+import Link from "next/link";
+
+export default function TopNav() {
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState<any>(null)
+    useEffect(() => {
+        const getCSRF = async () => {
+            // const res =  await d2xAPI.GET('auth/exchange/')
+            await arcadiaAPI.GET('util/csrf/')
+        }
+
+        const getUser = async () => {
+            arcadiaAPI.GET<any>("users/")
+            .then((res) => {
+                setIsLoggedIn(true)
+                setUser(res.user)
+                console.log(res.user)
+            })
+        }
+        getCSRF()
+        getUser()
+    }, [])
+    
+    const handleDemoLogIn = () => {
+        arcadiaAPI.POST("users/demo/login/", {})
+        .then(() => {
+            setIsLoggedIn(true)
+            toaster.create({
+                description: 'Demo Login Successful',
+                type: 'success'
+            })
+        })
+    }
 
 import { Avatar, Button, Drawer, IconButton } from "@chakra-ui/react";
 import { useEffect } from "react";
@@ -21,6 +61,9 @@ export default function TopNav() {
     return (
         <div id="top-nav">
             <div className="icon">
+                <Link href={'/'}>
+                    <img src="/logos/logo_white.svg" alt="" />
+                </Link>
                 <img src="/logos/logo_white.svg" alt="" />
             </div>
             <div className="hamburger">
@@ -44,12 +87,39 @@ export default function TopNav() {
                 <Drawer.Root>
                     <Drawer.Trigger asChild>
                         <Avatar.Root>
+                            {
+                                user ?
+                                    <Avatar.Image src={`/storage/preset-profile-pics/${user.picturePreset}.webp`} />
+                                :
+                                    <Avatar.Image src={'/sad.jpeg'} />
+                            }
                             <Avatar.Image src={'/sad.jpeg'} />
                         </Avatar.Root>
                     </Drawer.Trigger>
                     <Drawer.Positioner>
                         <Drawer.Content>
                             <Drawer.Header>
+                                <Avatar.Root>
+                                    {
+                                        user ?
+                                            <Avatar.Image src={`/storage/preset-profile-pics/${user.picturePreset}.webp`} />
+                                        :
+                                            <Avatar.Image src={'/sad.jpeg'} />
+                                    }
+                                </Avatar.Root>
+                                <p>{user ? user.username : ''}</p>
+                            </Drawer.Header>
+                            <Drawer.Body>
+                                {
+                                    !isLoggedIn &&
+                                        <Button onClick={() => handleDemoLogIn()}>
+                                            Login as Demo User
+                                        </Button>
+                                }
+                            </Drawer.Body>
+                            {/* <Drawer.Footer>
+                                Logout
+                            </Drawer.Footer> */}
                                 <p>My Profile</p>
                                 <Avatar.Root>
                                     <Avatar.Image src={'/sad.jpeg'} />
