a05ccb8c9876b7fbed82eadc74759a3fb811e1c5
Auston <nepgyah@gmail.com>
2026-03-02 01:43:19 -0600

    feat: add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,11 @@
+import '@/styles/pages/_not-found.scss';
+
 export default function NotFound() {
     return (
-        <div>
-            Not Found
+        <div id="page-not-found">
+            <h1 className='clr-arc-accent'>404</h1>
+            <p className="bold">Page Not Found</p>
+            <p className='clr-txt-fadded'>The page you requested got lost in the filer episodes.</p>
         </div>
     )
 }
