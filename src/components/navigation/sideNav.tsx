365eaa1d98ef85cfbc2127b24c3abc14eebb2b48
Auston <137665935+Nepgyah@users.noreply.github.com>
2026-03-10 00:50:34 -0500

    Feature/homepage
    * feat: add homepage content
    
    * feat: adjusted navigation positioning on desktop

--- src/components/navigation/sideNav.tsx
+++ src/components/navigation/sideNav.tsx
@@ -10,6 +10,11 @@ export default function SideNav(
 ) {
     return (
         <div id="side-nav">
+            <div className="icon">
+                <Link href={'/'}>
+                    <img src="/logos/logo_white.svg" alt="" />
+                </Link>
+            </div>
             <div className="links">
                 {
                     urlSet.map((url, idx) => (
