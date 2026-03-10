3a83fe3928e903f77467d355fdf23e6b84fc6396
Auston <nepgyah@gmail.com>
2026-03-10 18:28:34 -0500

    feat: add animation to character card

--- src/components/media/characters/character-card.tsx
+++ src/components/media/characters/character-card.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function CharacterCard(
     {
@@ -8,9 +11,15 @@ export default function CharacterCard(
         lSideTitle: string, lSideNote: string, lSideLink: string | null, lSideSrc: string,
         rSideTitle: string, rSideNote: string, rSideLink: string | null, rSideSrc: string
 }) {
+
+    const [sideSelected, setSideSelected] = useState<'left' | 'right' | null>(null)
+    
     return (
         <div className="character-card border-radius-sm card">
-            <img className="character-picture" src={lSideSrc} alt="" />
+            <div className="character-picture" onMouseEnter={() => setSideSelected('left')} onMouseLeave={() => setSideSelected(null)}>
+                <div className={`mask ${sideSelected == 'right' && 'selected'}`}></div>
+                <img src={lSideSrc} alt="" />
+            </div>
             <div className="names p-a-sm">
                 <div className="character">
                     <p>{lSideTitle}</p>
@@ -20,7 +29,7 @@ export default function CharacterCard(
                     {
                         rSideLink ?
                             <Link className="clickable" prefetch={false} href={rSideLink}>
-                                <p className="hover-underline">{rSideTitle}</p>
+                                <p className="hover-underline" onMouseEnter={() => setSideSelected('right')} onMouseLeave={() => setSideSelected(null)}>{rSideTitle}</p>
                             </Link>
                         :
                             <p className="hover-underline">{rSideTitle}</p>
@@ -28,10 +37,14 @@ export default function CharacterCard(
                     <p>Japanese</p>
                 </div>
             </div>
+
             {
                 rSideLink ?
                     <Link className="clickable" prefetch={false} href={rSideLink}>
-                        <img className="voice-actor-picture" src={rSideSrc} alt=""/>
+                        <div className="voice-actor-picture" onMouseEnter={() => setSideSelected('right')} onMouseLeave={() => setSideSelected(null)}>
+                            <div className={`mask ${sideSelected == 'left' && 'selected'}`}></div>
+                            <img src={rSideSrc} alt=""/>
+                        </div>
                     </Link>
                 :
                     <img className="voice-actor-picture" src={rSideSrc} alt=""/>
