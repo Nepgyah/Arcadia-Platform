8595b958506df8057fd45f6d57c6ae55201ea809
Auston Pawell <nepgyah@gmail.com>
2026-04-09 20:17:31 +0900

    feat: add server action for admin authentication

---
+++ src/utils/api/arcadiaAPI.ts
@@ -0,0 +1,77 @@
+import { cookies } from "next/headers";
+
+export class ArcadiaAPI {
+
+    async POST<T>(endpoint: string, body: any) : Promise<T> {
+        const api_endpoint = `${process.env.NEXT_PUBLIC_ARCADIA_API_URL}${endpoint}`;
+        const cookieStore = await cookies()
+        const access_token = cookieStore.get('access_token')?.value
+        const headers: Record<string, string> = {
+            "Content-Type": "application/json"
+        }
+
+        if (access_token) {
+            headers['authorization'] = `Bearer ${access_token}`
+        }
+
+        try {
+            const response = await fetch(
+                api_endpoint,
+                {
+                    method: 'POST',
+                    headers: headers,
+                    body: JSON.stringify(body)
+                }
+            )
+            
+            const data = await response.json()
+            if (response.ok) {
+                console.log(data)
+                return data
+            } else {
+                throw 'Error occured within the Arcadia API'
+            }
+        } catch {
+            throw 'Error occured attempting to call Arcadia REST POST endpoint'
+        }
+    }
+
+    async GraphQL<T>(query: any, variables = {}) : Promise<T> {
+        const api_endpoint = `${process.env.NEXT_PUBLIC_ARCADIA_GRAPH_URL}`;
+        const cookieStore = await cookies()
+        const access_token = cookieStore.get('access_token')?.value
+
+        const headers: Record<string, string> = {
+            "Content-Type": "application/json"
+        }
+
+        if (access_token) {
+            headers['authorization'] = `Bearer ${access_token}`
+        }
+
+        try {
+            const response = await fetch(
+                api_endpoint,
+                {
+                    method: 'POST',
+                    headers: headers,
+                    body: JSON.stringify({
+                        query,
+                        variables
+                    })
+                }
+            )
+            
+            const data = await response.json()
+            if (response.ok) {
+                return data
+            } else {
+                throw 'Error occured within the Arcadia API'
+            }
+        } catch {
+            throw 'Error occured attempting to call Arcadia GraphQL endpoint'
+        }
+    }
+}
+
+export const arcadiaAPI = new ArcadiaAPI()
