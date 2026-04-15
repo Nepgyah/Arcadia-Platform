46b5b5b4f633bb6ef943da56ddd86c55e2f2dad7
Auston Pawell <nepgyah@gmail.com>
2026-04-15 19:00:17 +0900

    feat: apply zod to arcadia search

--- src/app/arcadiaSearch.tsx
+++ src/app/arcadiaSearch.tsx
@@ -1,20 +1,42 @@
 'use client';
 
+import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import * as z from 'zod';
 import { Group, Button, Input } from "@chakra-ui/react";
 import Header from "@/components/custom/header";
+import { toaster } from "@/components/ui/toaster";
+
+const SearchQuery = z.object({
+    query: z.string().min(3, 'Please enter at least 3 letters')
+})
 
 export default function ArcadiaSearch() {
     const router = useRouter()
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
 
     const handleSearch = (e: React.SyntheticEvent) => {
         e.preventDefault()
-        setLoading(true)
-        router.push(`/search?query=${searchQuery}`)
+        try {
+            setLoading(true)
+            SearchQuery.parse({
+                query: searchQuery
+            })
+            router.push(`/search?query=${searchQuery}`)
+        } catch (error: any) {
+            if (error instanceof z.ZodError) {
+                if (inputRef.current) {
+                    inputRef.current.focus()
+                }
+                toaster.create({
+                    title: error.issues[0].message,
+                    type: 'error'
+                })
+            }
+        }
     }
 
     return (
@@ -22,7 +44,7 @@ export default function ArcadiaSearch() {
             <Header text="Search Arcadia" />
             <form onSubmit={handleSearch}>
                 <Group attached>
-                    <Input flex="1" placeholder="Bocchi the rock, Honkai, Machico, etc.." onChange={(e) => setSearchQuery(e.target.value)} />
+                    <Input ref={inputRef} id="search-input" flex="1" placeholder="Bocchi the rock, Honkai, Machico, etc.." onChange={(e) => setSearchQuery(e.target.value)} />
                     <Button loading={loading} type="submit" bg="bg.subtle" variant="outline" onClick={handleSearch}>
                         Search Arcadia
                     </Button>
