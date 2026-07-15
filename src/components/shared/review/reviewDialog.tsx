f2479fd8715542e44303b7685eaff6dda1888e8e
Auston <nepgyah@gmail.com>
2026-07-14 19:47:31 -0500

    feat: add score to review

--- src/components/shared/review/reviewDialog.tsx
+++ src/components/shared/review/reviewDialog.tsx
@@ -3,10 +3,11 @@
 import * as z from 'zod';
 import { POSTResponse } from "@/types/api";
 import { App, MediaReview, MediaReviewInput } from '@/types/base';
-import { Button, CloseButton, Dialog, Field, Portal, Textarea } from '@chakra-ui/react';
+import { Button, CloseButton, Dialog, Field, NativeSelect, Portal, Textarea } from '@chakra-ui/react';
 import { useContext, useEffect, useState } from 'react';
 import { CreateErrorToaster, CreateSuccessToaster } from '@/lib/helper/toasterHelpers';
 import MediaReviewContextWrapper, { MediaReviewContext } from '@/contexts/hasReviewContext';
+import SelectScore from '@/components/ui/selectScore';
 
 interface DialogProps {
     isOpen: boolean,
@@ -171,6 +172,27 @@ export default function ReviewDialog(
                                         }))
                                     }
                                 />
+                                <Field.Root>
+                                    <Field.Label>Score</Field.Label>
+                                    <NativeSelect.Root>
+                                        <NativeSelect.Field value={details.score} onChange={(e) => setDetails((prev) => ({
+                                            ...prev,
+                                            score: Number(e.target.value)
+                                        }))}>
+                                            <option value={10}>10 - Cinema</option>
+                                            <option value={9}>9 - Amazing</option>
+                                            <option value={8}>8 - Great</option>
+                                            <option value={7}>7 - Actually Good</option>
+                                            <option value={6}>6 - Good Trash</option>
+                                            <option value={5}>5 - Mid</option>
+                                            <option value={4}>4 - Watchable Trash</option>
+                                            <option value={3}>3 - Very Bad</option>
+                                            <option value={2}>2 - Appaling</option>
+                                            <option value={1}>1 - Actual Trash</option>
+                                            <option value={-1} disabled>Select Score</option>
+                                        </NativeSelect.Field>
+                                    </NativeSelect.Root>
+                                </Field.Root>
                                 <Field.HelperText>Minimum 25, Maximum 2000 characters</Field.HelperText>
                             </Field.Root>
                             </Dialog.Body>
